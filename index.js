fe048d59f8865816a67dd542bee890b6e34dddf0
Jean <jean.wachuka@student.moringaschool.com>
2024-04-10 14:35:49 +0300

    First class function lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2);
+};
+const selectingDrivers = [returnLastTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = function(multiplier) {
+  return function(fare) {
+    return fare * multiplier;
+  };
+};
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+const selectDifferentDrivers = function(drivers, selectFunction) {
+  return selectFunction(drivers);
+};
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const lastTwoDrivers = selectDifferentDrivers(drivers, returnLastTwoDrivers);
+console.log(lastTwoDrivers); 
